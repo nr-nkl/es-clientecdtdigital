d8a00ec630b1e9bed6ff4e8956ceafa3e3fdd312
N R <143750294+nar-ran@users.noreply.github.com>
2024-03-29 00:04:35 -0500

    Controller GraphQL

--- src/main/java/com/mibanco/clientecdtdigital/es/controller/graphql/ClienteCDTDigitalGraphqlController.java
+++ src/main/java/com/mibanco/clientecdtdigital/es/controller/graphql/ClienteCDTDigitalGraphqlController.java
@@ -5,10 +5,9 @@ import com.mibanco.clientecdtdigital.es.gen.type.ClienteCDTDigitalType;
 import com.mibanco.clientecdtdigital.es.service.impl.ClienteCDTDigitalServiceImpl;
 import com.mibanco.clientecdtdigital.es.utils.exception.ApplicationException;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
 import org.eclipse.microprofile.graphql.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,73 +21,66 @@ public class ClienteCDTDigitalGraphqlController {
     ClienteCDTDigitalServiceImpl clienteCDTDigitalServiceImpl;
 
     @Mutation("crearCliente")
-    @Transactional
-    public Response crearClienteCDTDigital(ClienteCDTDigitalType clienteCDTDigitalType) {
+    public ClienteCDTDigitalType crearClienteCDTDigital(@Name("cliente") ClienteCDTDigitalType clienteCDTDigitalType) {
         LOG.info("Inicia el proceso crearClienteCDTDigital GraphQL Controller.");
+        ClienteCDTDigitalType clienteCDTDigitalTypeCreado = null;
         try{
-            clienteCDTDigitalServiceImpl.crearClienteCDTDigital(clienteCDTDigitalType);
+            clienteCDTDigitalTypeCreado = clienteCDTDigitalServiceImpl.crearClienteCDTDigital(clienteCDTDigitalType);
         }catch (ApplicationException e){
             LOG.error(Constant.ERROR_SERVICIO + e.getMessage() + " crearClienteCDTDigital GraphQL Controller.");
-            return Response.status(Response.Status.BAD_REQUEST).entity(clienteCDTDigitalType).build();
         }
         LOG.info("Termina el proceso crearClienteCDTDigital GraphQL Controller.");
-        return  Response.status(Response.Status.CREATED).entity(clienteCDTDigitalType).build();
+        return clienteCDTDigitalTypeCreado;
     }
 
     @Query("buscarClienteId")
-    public Response buscarClienteCDTDigitalId(Integer idCliente) {
+    public ClienteCDTDigitalType buscarClienteCDTDigitalId(@Name("id") Integer idCliente) {
         LOG.info("Inicia el proceso buscarClienteCDTDigitalId GraphQL Controller.");
         ClienteCDTDigitalType clienteCDTDigitalType = null;
         try{
             clienteCDTDigitalType = clienteCDTDigitalServiceImpl.buscarClienteCDTId(idCliente);
         }catch (ApplicationException e){
             LOG.error(Constant.ERROR_SERVICIO+e.getMessage()+" buscarClienteCDTPorId GraphQL Controller.");
-            return Response.status(Response.Status.BAD_REQUEST).entity(clienteCDTDigitalType).build();
         }
         LOG.info("Termina el proceso buscarClienteCDTPorId GraphQL Controller.");
-        return Response.status(Response.Status.FOUND).entity(clienteCDTDigitalType).build();
+        return clienteCDTDigitalType;
     }
 
     @Query("listarClientes")
-    public Response listarClientesCDTDigital() {
+    public List<ClienteCDTDigitalType> listarClientesCDTDigital() {
         LOG.info("Inicia el proceso listarClientesCDTDigital GraphQL Controller.");
         List<ClienteCDTDigitalType> clienteCDTDigitalTypeList = null;
         try{
             clienteCDTDigitalTypeList = clienteCDTDigitalServiceImpl.listarClientesCDTDigital();
         }catch (ApplicationException e){
             LOG.error(Constant.ERROR_SERVICIO+e.getMessage()+" listarClientesCDTDigital GraphQL Controller.");
-            return Response.status(Response.Status.BAD_REQUEST).entity(clienteCDTDigitalTypeList).build();
         }
         LOG.info("Termina el proceso listarClientesCDTDigital GraphQL Controller.");
-        return Response.status(Response.Status.FOUND).entity(clienteCDTDigitalTypeList).build();
+        return clienteCDTDigitalTypeList;
     }
 
     @Mutation("eliminarCliente")
-    @Transactional
-    public Response eliminarClienteCDTDigitalId(Integer idCliente) {
+    public void eliminarClienteCDTDigitalId(@Name("id") Integer idCliente) {
         LOG.info("Inicia el proceso eliminarClienteCDTDigitalId GraphQL Controller.");
         try{
             clienteCDTDigitalServiceImpl.eliminarClienteCDTDigitalId(idCliente);
         }catch (ApplicationException e){
             LOG.error(Constant.ERROR_SERVICIO+e.getMessage()+" eliminarClienteCDTDigitalId GraphQL Controller.");
-            return Response.status(Response.Status.BAD_REQUEST).build();
         }
         LOG.info("Termina el proceso eliminarClienteCDTDigitalId GraphQL Controller.");
-        return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @Mutation("actualizarCliente")
-    @Transactional
-    public Response actualizarClienteCDTId(Integer idCliente, ClienteCDTDigitalType clienteCDTDigitalType) {
+    public ClienteCDTDigitalType actualizarClienteCDTId(@Name("id")Integer idCliente, ClienteCDTDigitalType clienteCDTDigitalType) {
         LOG.info("Inicia el proceso actualizarClienteCDTId GraphQL Controller.");
+        ClienteCDTDigitalType clienteCDTDigitalTypeActualizado = null;
         try{
-            clienteCDTDigitalServiceImpl.actualizarClienteCDTId(idCliente, clienteCDTDigitalType);
+            clienteCDTDigitalTypeActualizado = clienteCDTDigitalServiceImpl.actualizarClienteCDTId(idCliente, clienteCDTDigitalType);
         }catch (ApplicationException e){
             LOG.error(Constant.ERROR_SERVICIO+e.getMessage()+" actualizarClienteCDTId GraphQL Controller.");
-            return Response.status(Response.Status.BAD_REQUEST).build();
         }
         LOG.info("Termina el proceso actualizarClienteCDTId GraphQL Controller.");
-        return Response.status(Response.Status.OK).build();
+        return clienteCDTDigitalTypeActualizado;
     }
 
 
